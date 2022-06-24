5f47f486cff3444f196e329d519926d53dd8eb75
Kris Fortney <kfortney1@charter.net>
2022-06-23 20:02:04 -0500

    updated JavaScript

--- script.js
+++ script.js
@@ -7,11 +7,22 @@ function TestHi() {
 
     //Test is input value is "hi"
     if (inputElement.value === "hi")
-    {
+     {
         alert("Correct");
     }
     else {
         alert("Incorrect");
-        inputElement.style.border = "2px solid red"
+        inputElement.style.border = "2px solid red";
+    }
+}
+
+function generateElements() {
+    alert("Function is called");
+    let tech = ["HTML", "CSS", "JavaScript"];
+    let bulletlist = document.getElementById("testGeneration");
+    for (let i = 0; i <tech.length; i++) {
+        let newBullet = document.createElement("li");
+        newBullet.innerHTML = "Learn " + tech[i];
+        bulletlist.appendChild(newBullet);
     }
 }
